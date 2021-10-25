d764dedfc00dc5f6fe67f55953ebd9ec4a0eb8fd
Yeahboi12356 <89899637+Yeahboi12356@users.noreply.github.com>
2021-10-25 21:39:59 +0800

    Add files via upload

---
+++ XAMPP.bat
@@ -0,0 +1,4 @@
+@echo off
+echo "Downloading XAMPP 64bit"
+powershell -Command "invoke-WebReqest https://downloadsapachefriends.global.ssl.fastly.net/8.0.12/xampp-windows-x64-8.0.12-0-VS16-installer.exe -Outfole XAMPP.exe"
+echo "done"
