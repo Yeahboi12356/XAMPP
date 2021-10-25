c4c18900da555b92ed31aa1eb6a9b7151abd218e
Yeahboi12356 <89899637+Yeahboi12356@users.noreply.github.com>
2021-10-25 21:53:14 +0800

    Update XAMPP.bat

--- XAMPP.bat
+++ XAMPP.bat
@@ -1,4 +1,4 @@
 @echo off
-echo "Downloading XAMPP 64bit"
+echo Downloading XAMPP 64bit
 powershell -Command "Invoke-WebReqest https://downloadsapachefriends.global.ssl.fastly.net/8.0.12/xampp-windows-x64-8.0.12-0-VS16-installer.exe -Outfile XAMPP.exe"
-echo "done"
+echo done
